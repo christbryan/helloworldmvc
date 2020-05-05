9a6a30cf2813fc923b725df58871b9bde0ad7923
christbryan <58194263+christbryan@users.noreply.github.com>
2020-05-06 00:07:03 +0100

    phase2

---
+++ helloworldmaven/helloworldmvc.contract/src/test/java/org/helloworldmvc/contract/AppTest.java
@@ -0,0 +1,20 @@
+package org.helloworldmvc.contract;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
